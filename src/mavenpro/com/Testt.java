1d76262a83e09815f8a7be58c795c94f35693ae4
Sandhyashree <msandya.sugu@gmail.com>
2023-03-03 13:59:51 +0530

    Sample

---
+++ src/mavenpro/com/Testt.java
@@ -0,0 +1,11 @@
+package mavenpro.com;
+
+public class Testt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "sandhya";
+		System.out.println(s);
+	}
+
+}
